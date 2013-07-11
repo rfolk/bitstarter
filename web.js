a24215f0b8cf43100554e56b2f2d464fc0f96766
Russell Folk <rusty@russellfolk.me>
2013-07-11 03:05:28 +0000

    Updated web.js to use 'index.html'

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buffer = new Buffer ();
-  response.send(buffer.toString('utf-8' , fs.readFileSync("index.html")));
+  response.send(buffer.toString( fs.readFileSync( "index.html" ) , "utf-8" ));
 //  response.send('Hello World 2!');
 });
 
