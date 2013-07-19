cf259a821b5d8b08b1b1ce90d8f6f31f81c6979a
Russell Folk <rfolk@russellfolk.com>
2013-07-19 00:19:46 -0600

    Added a watch to check directories for CSS and JS

--- web.js
+++ web.js
@@ -1,15 +1,20 @@
 var express = require('express');
+var fs = require('fs');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var buffer = new Buffer (fs.readFileSync( "index.html" , "utf-8" ));
-  response.send(buffer.toString( "utf-8" ));
+  var html = fs.readFileSync( htmlfile ).toString();
+  response.send( html );
 //  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 8080;
+app.use("/css",express.static(__dirname+"/css"));
+app.use("/font",express.static(__dirname+"/font"));
+app.use("/img",express.static(__dirname+"/img"));
+app.use("/js",express.static(__dirname+"/js"));
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
